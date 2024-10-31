300c97581094d416e2e4af288fb961432badf764
pauloricardomeritello99@gmail.com <seuemail@example.com>
2024-10-31 01:46:10 -0300

    Criando as rotas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './navegacao/home/home.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'contato', component: ContatoComponent },
+    { path: 'sobre', component: SobreComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
