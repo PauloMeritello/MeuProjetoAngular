a317dd2d72e150a179882e2b1dc9e320600eda40
pauloricardomeritello99@gmail.com <seuemail@example.com>
2024-11-04 20:45:29 -0300

    Ultimo commit do curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
+import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+import { FormsModule } from '@angular/forms';
+import { ProdutoService } from './produtos/produtos.service';
 
 
 @NgModule({
@@ -15,12 +23,16 @@ import { FooterComponent } from './navegacao/footer/footer.component';
     MenuComponent,
     HomeComponent,
     FooterComponent,
+    DataBindingComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [
+    ProdutoService,
+    provideHttpClient(withInterceptorsFromDi()),
     {provide: APP_BASE_HREF, useValue: '/'}
   ],
   bootstrap: [AppComponent]
---
+++ src/app/demos/data-binding/data-binding.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  template: `
+    <p>
+      data-binding works!
+    </p>
+  `,
+  styles: ``
+})
+export class DataBindingComponent {
+
+}
